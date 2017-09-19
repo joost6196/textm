315a422983b7331eec308dbeaa0a610d53c586a2
Pieter Wolfert <pieter@pieterwolfert.com>
2017-09-19 22:09:43 +0200

    lazy cleaning of file

--- assignment2/extractor.py
+++ assignment2/extractor.py
@@ -1,33 +1,47 @@
-textfile = open("/home/pieter/projects/textm/assignment2/data/AmericanGazetteer_1762_6pages.txt", "r")
-outputfile = open("/home/pieter/projects/textm/assignment2/output.txt", "w")
-lines = textfile.readlines()
+import re
 
-newfile = []
+#author Pieter Wolfert s4220366
 
-for line in lines:
-    if ". " in line:
-        line = line.replace('. ', '.\n')
-        newfile.append(line)
-    else:
-        newfile.append(line)
-    #newline = line.split('.')
+def loadfiles():
+    outputfile = open("/home/pieter/projects/textm/assignment2/output.txt", "w")
+    textfile = open("/home/pieter/projects/textm/assignment2/data/AmericanGazetteer_1762_6pages.txt", "r")
+    lines = textfile.read()
+    return lines, outputfile, textfile
 
-clean = []
+def main():
+    lines, outputfile, textfile = loadfiles()
+    newfile = []
+    for cha in lines:
+        if cha == '-' :
+            newfile.append(' ')
+        else:
+            newfile.append(cha)
+    seconditer = []
 
-for line in newfile:
-    if len(line.strip()) == 0:
-        print line
-    else:
-        line = line.replace('-\n', '')
-        line = line.replace(' \n', '')
-        clean.append(line)
+    #remove the end /splitted sentences
+    for i, cha in enumerate(newfile):
+        if i < len(newfile)-1:
+            if newfile[i] == '\n' and newfile[i-1] != '\n' and newfile[i+1] != '\n':
+                seconditer.append(' ')
+            else:
+                seconditer.append(cha)
+    thirditer = []
+    #split text
 
-print len(clean)
+    for i, cha in enumerate(seconditer):
+        if cha == '.' and seconditer[i+1] == ' ':
+            thirditer.append(cha)
+            thirditer.append('\n')
+        else:
+            thirditer.append(cha)
 
-for c in clean:
-    outputfile.write(c)
+    for j in thirditer:
+        outputfile.write(j)
+    close(textfile, outputfile)
 
+def close(textfile, outputfile):
+    textfile.close()
+    outputfile.close()
 
-
-textfile.close()
-outputfile.close()
+if __name__ == '__main__':
+    main()
